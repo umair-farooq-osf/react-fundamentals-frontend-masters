72d3741b4c40560f8a658b1d277b142dd47010c1
Umair Farooq <umair.farooq@osf-commerce.com>
2023-08-17 17:20:59 +0500

    Added SearchParams component, usage of useState hook

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
     return (
         <div id="my-app">
             <h1>Adopt Me!</h1>
-            <Pet name="Luna" animal="Dog" breed="Havanese" />
-            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-            <Pet name="Doink" animal="Cat" breed="Mixed" />
+            <SearchParams />
         </div>
     )
 };
