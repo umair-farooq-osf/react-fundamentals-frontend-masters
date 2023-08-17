d6ce7ebcc09208c035442ae733ff491e2b38188e
Umair Farooq <umair.farooq@osf-commerce.com>
2023-08-17 18:57:31 +0500

    Tried useDebugValue hook

--- adopt-me/src/useBreedList.js
+++ adopt-me/src/useBreedList.js
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 const localCache = {};
 
 export default function useBreedList(animal) {
   const [breedList, setBreedList] = useState([]);
   const [status, setStatus] = useState("unloaded");
+
+  useDebugValue("Hi from useBreedList");
+  
   useEffect(() => {
     if (!animal) {
       setBreedList([]);
