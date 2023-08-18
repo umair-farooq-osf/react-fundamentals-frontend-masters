cdbe408df1eed33418e5b173bffa25557c312664
Umair Farooq <umair.farooq@osf-commerce.com>
2023-08-18 15:34:36 +0500

    Converted Details component from Function component to Class component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,9 +1,42 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-const Details = () => {
-    const { id } = useParams();
+class Details extends Component {
+    constructor(props) {
+        super(props);
 
-    return <h2>{id}</h2>;
+        this.state = { loading: true };
+    }
+
+    async componentDidMount() {
+        const { id } = this.props.params;
+        const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json.pets[0]));
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <h2>Loading...</h2>;
+        }
+
+        const { animal, breed, city, state, description, name } = this.state;
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {city}, {state}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+const WrappedDetails = () => {
+    const params = useParams();
+    return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
