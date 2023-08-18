5896cb2a6e1aa3cce0ca9886ce7664c22238f0f5
Umair Farooq <umair.farooq@osf-commerce.com>
2023-08-18 17:03:08 +0500

    Created ErrorBoundary & used in Details component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,40 +1,48 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
-    state = { loading: true };
+  state = { loading: true };
 
-    async componentDidMount() {
-        const { id } = this.props.params;
-        const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
-        const json = await res.json();
-        this.setState(Object.assign({ loading: false }, json.pets[0]));
+  async componentDidMount() {
+    const { id } = this.props.params;
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
     }
 
-    render() {
-        if (this.state.loading) {
-            return <h2>Loading...</h2>;
-        }
-
-        const { animal, breed, city, state, description, name, images } = this.state;
-        return (
-            <div className="details">
-                <Carousel images={images} />
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{animal} - {breed} - {city}, {state}</h2>
-                    <button>Adopt {name}</button>
-                    <p>{description}</p>
-                </div>
-            </div>
-        );
-    }
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 const WrappedDetails = () => {
-    const params = useParams();
-    return <Details params={params} />;
+  const params = useParams();
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
